cd61c4a9fa8535b703cee41731d8d0fa82e72000
Matthew Beale <mixonic@synitech.com>
2008-08-11 17:56:38 -0400

    Allow for no default database (use _directory when applicable)

--- lib/active_record/base.rb
+++ lib/active_record/base.rb
@@ -1,19 +1,26 @@
 require 'active_record'
 
-class ActiveRecord::Base
+module ActiveRecord
+  
+  class Base
 
-  alias :_establish_connection :establish_connection
+  class << self
+    alias :_establish_connection :establish_connection
+  end
+  
   def self.establish_connection( spec = nil )
     case spec
       when nil
         begin
-          _establish_connection( spec )
-        rescue AdaptedNotSpecified
-          _establish_connection( spec+'_directory' )
+          _establish_connection( RAILS_ENV )
+        rescue AdapterNotSpecified
+          _establish_connection( RAILS_ENV+'_directory' )
         end
       else
         _establish_connection( spec )
     end
+  end
+  
   end
 
 end
