ef9d29099d734c2e63ddc135c8676000e170fcac
Matthew Beale <mixonic@synitech.com>
2008-08-15 12:16:53 -0400

    More merb compatibility

--- shard_the_love.rb
+++ shard_the_love.rb
@@ -1,12 +1,13 @@
 # Set up some ENV for merb/rails compatibility, load the library
 
-if defined?(Rails)
-  ROOT = RAILS_ROOT
-  ENV = RAILS_ENV
-elsif defined?(Merb)
-  ROOT = Merb.root
-  ENV = (Merb.env == 'rake' ? 'development' : Merb.env)
-end
-
 require File.dirname(__FILE__) + '/lib/active_record/base'
 require File.dirname(__FILE__) + '/lib/shard_the_love'
+
+if defined?(Rails)
+  ShardTheLove::ROOT = RAILS_ROOT
+  ShardTheLove::ENV = RAILS_ENV
+elsif defined?(Merb)
+  ShardTheLove::ROOT = Merb.root
+  ShardTheLove::ENV = (Merb.env == 'rake' ? 'development' : Merb.env)
+end
+
